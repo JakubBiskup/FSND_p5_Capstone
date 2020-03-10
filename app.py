89608a598cc498c1b3b5d9ca851ecb68fb5e6377
Jakub Biskup <biskup.jakub@gmail.com>
2020-03-10 19:43:20 +0100

    Add basic club collection view

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app=Flask(__name__)
 def index():
   return render_template('pages/homepage.html')
 
+@app.route('/clubcollection')
+def get_all_games():
+  return render_template('pages/clubcollection.html')
+
 
 
 if __name__ == '__main__':
