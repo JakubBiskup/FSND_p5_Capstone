0c572d8e240a42041b21fe42dd4b62eb64afd549
Jakub Biskup <biskup.jakub@gmail.com>
2020-03-15 15:18:14 +0100

    Add models

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from models import db, db_path, setup_db, Game
 
 app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = db_path
 
+setup_db(app, db_path)
+'''
+@app.route('/getfirstgame') ######test endpoint, delete later
+def get_first_game_from_db():
+  try:
+    games=Game.query.all()
+    game=games[0]
+    title=game.title
+    link=game.link
+  except Exception as e:
+    print(e)
+    return jsonify({'success':False}),500
+  return jsonify({'success':True,'title':title,'link':link}), 200
+'''
 @app.route('/')
 def index():
   return render_template('pages/home.html')
